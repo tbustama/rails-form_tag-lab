2af405ad21c4f4194852ae6403de3c378d9f9fff
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-13 09:29:58 -0800

    commit tree 56857543585925fa1fce9e6a58fa43feb6a07989

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,19 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params["student"]["first_name"]
+    @student.last_name = params["student"]["last_name"]
+    if @student.save
+        redirect_to @student
+    else  
+        render 'new'
+    end
+end
+
 end
